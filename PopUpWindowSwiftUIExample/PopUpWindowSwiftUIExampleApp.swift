a69a5c111bf4741cb576d599241d0ea01407af44
John Codeos <johncodeos@gmail.com>
2020-11-05 00:19:57 +0200

    Initial Commit

---
+++ PopUpWindowSwiftUIExample/PopUpWindowSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopUpWindowSwiftUIExampleApp.swift
+//  PopUpWindowSwiftUIExample
+//
+//  Created by John Codeos on 11/5/20.
+//
+
+import SwiftUI
+
+@main
+struct PopUpWindowSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
